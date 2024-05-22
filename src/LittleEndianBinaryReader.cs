2cc3a804a09d3b57bf6871cfac7f1d980319cffa
Hakan Lindestaf <hakan@lindestaf.com>
2024-05-21 22:33:08 -0500

    Added LittleEndian binary functions

---
+++ src/LittleEndianBinaryReader.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Haukcode.Network
+{
+    public class LittleEndianBinaryReader
+    {
+        private int readPosition = 0;
+        private readonly ReadOnlyMemory<byte> buffer;
+
+        public LittleEndianBinaryReader(ReadOnlyMemory<byte> buffer)
+        {
+            this.buffer = buffer;
+        }
+
+        public short ReadInt16()
+        {
+            var span = this.buffer.Span;
+            byte b2 = span[this.readPosition++];
+            byte b1 = span[this.readPosition++];
+
+            return (short)((b1 << 8) | b2);
+        }
+
+        public ushort ReadUInt16()
+        {
+            var span = this.buffer.Span;
+            byte b2 = span[this.readPosition++];
+            byte b1 = span[this.readPosition++];
+
+            return (ushort)((b1 << 8) | b2);
+        }
+
+        public int ReadInt32()
+        {
+            var span = this.buffer.Span;
+            byte b4 = span[this.readPosition++];
+            byte b3 = span[this.readPosition++];
+            byte b2 = span[this.readPosition++];
+            byte b1 = span[this.readPosition++];
+
+            return (b1 << 24) | (b2 << 16) | (b3 << 8) | b4;
+        }
+
+        public uint ReadUInt32()
+        {
+            var span = this.buffer.Span;
+            byte b4 = span[this.readPosition++];
+            byte b3 = span[this.readPosition++];
+            byte b2 = span[this.readPosition++];
+            byte b1 = span[this.readPosition++];
+
+            return (uint)((b1 << 24) | (b2 << 16) | (b3 << 8) | b4);
+        }
+
+        public byte[] ReadBytes(int bytes)
+        {
+            var span = this.buffer.Slice(this.readPosition, bytes);
+
+            this.readPosition += bytes;
+
+            return span.ToArray();
+        }
+
+        public byte[] ReadBytes()
+        {
+            var span = this.buffer[this.readPosition..];
+
+            this.readPosition += span.Length;
+
+            return span.ToArray();
+        }
+
+        public bool VerifyBytes(byte[] bytes)
+        {
+            var span = this.buffer.Slice(this.readPosition, bytes.Length).Span;
+            this.readPosition += bytes.Length;
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (span[i] != bytes[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public Guid ReadGuid()
+        {
+            var input = this.buffer.Slice(readPosition).Span;
+            this.readPosition += 16;
+
+            return new Guid(new byte[] {
+                input[3],
+                input[2],
+                input[1],
+                input[0],
+
+                input[5],
+                input[4],
+
+                input[7],
+                input[6],
+
+                input[8],
+                input[9],
+
+                input[10],
+                input[11],
+                input[12],
+                input[13],
+                input[14],
+                input[15]
+            });
+        }
+
+        public string ReadString(int bytes)
+        {
+            var span = this.buffer.Slice(this.readPosition, bytes).Span;
+            int terminatorIndex = span.IndexOf((byte)0);
+            if (terminatorIndex == -1)
+                terminatorIndex = bytes;
+
+            this.readPosition += bytes;
+
+            return Encoding.UTF8.GetString(span[..terminatorIndex]);
+        }
+
+        public string ReadString()
+        {
+            var span = this.buffer.Slice(this.readPosition).Span;
+            int terminatorIndex = span.IndexOf((byte)0);
+            if (terminatorIndex == -1)
+                terminatorIndex = this.buffer.Length - this.readPosition;
+
+            this.readPosition += terminatorIndex + 1;
+
+            return Encoding.UTF8.GetString(span[..terminatorIndex]);
+        }
+
+        public byte ReadByte()
+        {
+            return this.buffer.Span[this.readPosition++];
+        }
+    }
+}
---
+++ src/LittleEndianBinaryWriter.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Haukcode.Network
+{
+    public class LittleEndianBinaryWriter
+    {
+        private int writePosition = 0;
+        private readonly Memory<byte> buffer;
+
+        public LittleEndianBinaryWriter(Memory<byte> buffer)
+        {
+            this.buffer = buffer;
+        }
+
+        public int WrittenBytes => this.writePosition;
+
+        public Memory<byte> Memory => this.buffer.Slice(this.writePosition);
+
+        public void WriteByte(byte value)
+        {
+            var span = buffer.Span;
+
+            span[this.writePosition++] = (byte)value;
+        }
+
+        public void WriteInt16(short value)
+        {
+            var span = buffer.Span;
+
+            span[this.writePosition++] = (byte)value;
+            span[this.writePosition++] = (byte)(value >> 8);
+        }
+
+        public void WriteUInt16(ushort value)
+        {
+            var span = buffer.Span;
+
+            span[this.writePosition++] = (byte)value;
+            span[this.writePosition++] = (byte)(value >> 8);
+        }
+
+        public void WriteInt32(int value)
+        {
+            var span = buffer.Span;
+
+            span[this.writePosition++] = (byte)value;
+            span[this.writePosition++] = (byte)(value >> 8);
+            span[this.writePosition++] = (byte)(value >> 16);
+            span[this.writePosition++] = (byte)(value >> 24);
+        }
+
+        public void WriteUInt32(uint value)
+        {
+            var span = buffer.Span;
+
+            span[this.writePosition++] = (byte)value;
+            span[this.writePosition++] = (byte)(value >> 8);
+            span[this.writePosition++] = (byte)(value >> 16);
+            span[this.writePosition++] = (byte)(value >> 24);
+        }
+
+        public void WriteBytes(byte[] bytes)
+        {
+            bytes.CopyTo(this.buffer[this.writePosition..].Span);
+
+            this.writePosition += bytes.Length;
+        }
+
+        public void WriteBytes(ReadOnlyMemory<byte> bytes)
+        {
+            bytes.Span.CopyTo(this.buffer[this.writePosition..].Span);
+
+            this.writePosition += bytes.Length;
+        }
+
+        public void WriteString(string value, int length)
+        {
+            //FIXME
+            WriteBytes(Encoding.UTF8.GetBytes(value));
+            WriteBytes(Enumerable.Repeat((byte)0, length - value.Length).ToArray());
+        }
+
+        private byte[] GuidToByteArray(Guid input)
+        {
+            var bytes = input.ToByteArray();
+
+            return new byte[] {
+                bytes[3],
+                bytes[2],
+                bytes[1],
+                bytes[0],
+
+                bytes[5],
+                bytes[4],
+
+                bytes[7],
+                bytes[6],
+
+                bytes[8],
+                bytes[9],
+
+                bytes[10],
+                bytes[11],
+                bytes[12],
+                bytes[13],
+                bytes[14],
+                bytes[15]
+            };
+        }
+
+        public void WriteGuid(Guid value)
+        {
+            // Fixme
+            var bytes = GuidToByteArray(value);
+
+            WriteBytes(bytes);
+        }
+    }
+}
